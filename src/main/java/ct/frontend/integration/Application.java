12dfd8eeb1c5f5c0c483282459ac6794aa8230e0
raiseltwice <deepbreathing888@gmail.com>
2021-01-06 08:26:50 +0200

    Adjustments

--- src/main/java/ct/frontend/integration/Application.java
+++ src/main/java/ct/frontend/integration/Application.java
@@ -5,13 +5,11 @@ package ct.frontend.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
